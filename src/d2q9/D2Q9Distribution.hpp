e820c488f12c368f92a096aca97d857ec519a56f
Carlos Fernando Baptista <cfd.baptista@gmail.com>
2024-10-28 14:57:45 +0100

    Make directions private

--- src/d2q9/D2Q9Distribution.hpp
+++ src/d2q9/D2Q9Distribution.hpp
@@ -15,6 +15,15 @@ public:
     static constexpr auto dimension() -> std::size_t;
     static constexpr auto size() -> std::size_t;
 
+    auto computeDensity() const -> Scalar;
+    auto computeMomentum() const -> std::array<Scalar, 2>;
+    static auto computeVelocity(const Scalar& density, const std::array<Scalar, 2>& momentum)
+        -> std::array<Scalar, 2>;
+
+private:
+    static constexpr std::size_t dimension_{2};
+    static constexpr std::size_t size_{9};
+
     static constexpr std::size_t center{0};
     static constexpr std::size_t right{1};
     static constexpr std::size_t top{2};
@@ -24,15 +33,6 @@ public:
     static constexpr std::size_t topLeft{6};
     static constexpr std::size_t bottomLeft{7};
     static constexpr std::size_t bottomRight{8};
-
-    auto computeDensity() const -> Scalar;
-    auto computeMomentum() const -> std::array<Scalar, 2>;
-    static auto computeVelocity(const Scalar& density, const std::array<Scalar, 2>& momentum)
-        -> std::array<Scalar, 2>;
-
-private:
-    static constexpr std::size_t dimension_{2};
-    static constexpr std::size_t size_{9};
 
     std::array<Scalar, D2Q9Distribution<Scalar>::size_> distribution_;
 };
